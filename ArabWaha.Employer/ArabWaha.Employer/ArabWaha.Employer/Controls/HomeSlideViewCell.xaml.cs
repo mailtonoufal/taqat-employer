ac51406c83db4d895c5ea779e1cc6bf332a3ac5e
gauravgupta1990 <gaurav.gupta@dotsquares.com>
2017-06-29 18:49:55 +0530

    Committed with carousal view on home screen and Integrated Announcements.

---
+++ ArabWaha.Employer/ArabWaha.Employer/ArabWaha.Employer/Controls/HomeSlideViewCell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ArabWaha.Employer.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeSlideViewCell : View
+    {
+        public HomeSlideViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
